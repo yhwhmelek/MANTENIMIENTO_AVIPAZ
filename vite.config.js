2e21a80c35031a659afe44e34d04df46ffd55558
Cristian Changoluisa <yhwhmelek@gmail.com>
2026-09-16 09:59:12 -0500

    actualziacion con server  avipaz

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['mantenimiento.guemultech.com'],
+    allowedHosts: ['mantenimiento.guemultech.com', 'mantenimientoavipaz.guemultech.com'],
   },
 })
